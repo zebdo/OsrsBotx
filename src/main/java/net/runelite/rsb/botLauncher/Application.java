464f361dc1e592d552bc09792192a0400121fea6
rxe42 <109221671+rxe42@users.noreply.github.com>
2022-09-01 21:08:51 +0100

    comment out clojure and CLI for now

--- src/main/java/net/runelite/rsb/botLauncher/Application.java
+++ src/main/java/net/runelite/rsb/botLauncher/Application.java
@@ -26,12 +26,12 @@ public class Application {
 	 * @throws Throwable	Any error that might be thrown
 	 */
 	public static void main(final String[] args) throws Throwable {
-		JnREPL.startRepl();
+		//JnREPL.startRepl();
 		preParser = new ArgumentPreParser(args);
 		if (preParser.contains("--bot-runelite")) {
 			addBot(preParser.contains("--headless"));
 			checkForCacheAndLoad();
-			CLIHandler.handleCLI();
+			//CLIHandler.handleCLI();
 		} else {
 			net.runelite.client.RuneLite.main(args);
 		}
