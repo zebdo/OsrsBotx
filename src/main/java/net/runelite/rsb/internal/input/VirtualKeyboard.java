c0fa1f3fb3442b6568a67fbfee63589a79041dd8
rxe42 <109221671+rxe42@users.noreply.github.com>
2023-01-16 11:23:38 +0000

    kill pointless KeyListener

--- src/main/java/net/runelite/rsb/internal/input/VirtualKeyboard.java
+++ src/main/java/net/runelite/rsb/internal/input/VirtualKeyboard.java
@@ -2,51 +2,26 @@ package net.runelite.rsb.internal.input;
 
 import lombok.extern.slf4j.Slf4j;
 
-import net.runelite.client.input.KeyListener;
 import net.runelite.rsb.internal.client_wrapper.RSClient;
 
-import java.applet.Applet;
-import java.awt.*;
-import java.awt.event.FocusEvent;
+import java.awt.Toolkit;
+import java.awt.EventQueue;
 import java.awt.event.KeyEvent;
-
+import java.awt.event.FocusEvent;
 
 @Slf4j
-@SuppressWarnings("removal")
-public class VirtualKeyboard implements KeyListener {
-
+public class VirtualKeyboard {
     private RSClient proxy;
 
     public VirtualKeyboard(RSClient proxy) {
         this.proxy = proxy;
     }
 
-    public void keyPressed(KeyEvent e) {
-    }
-
-    public void keyReleased(KeyEvent e) {
-    }
-
-    public void keyTyped(KeyEvent e) {
-    }
-
     public void sendEvent(KeyEvent e) {
-
-        if (e.getID() == KeyEvent.KEY_TYPED) {
-            keyTyped(e);
-        }
-
-        if (e.getID() == KeyEvent.KEY_PRESSED) {
-            keyPressed(e);
-        }
-
-        if (e.getID() == KeyEvent.KEY_RELEASED) {
-            keyReleased(e);
-        }
-
         if (!proxy.getCanvas().isFocusOwner()) {
             EventQueue eventQueue = Toolkit.getDefaultToolkit().getSystemEventQueue();
-            eventQueue.postEvent(new FocusEvent(proxy.getCanvas(), FocusEvent.FOCUS_GAINED));
+            eventQueue.postEvent(new FocusEvent(proxy.getCanvas(),
+                                                FocusEvent.FOCUS_GAINED));
         }
 
         proxy.getCanvas().dispatchEvent(e);
